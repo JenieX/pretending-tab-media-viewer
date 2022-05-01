543d99f00f189386d30b51d6a54788a101d7792b
FlowerForWar <102888858+FlowerForWar@users.noreply.github.com>
2022-05-01 07:12:57 +0300

    Update pretending-tab-images-viewer.user.js

--- pretending-tab-images-viewer.user.js
+++ pretending-tab-images-viewer.user.js
@@ -1,38 +1,120 @@
 // ==UserScript==
 // @name           Pretending Tab - Images Viewer
 // @namespace      PretendingTab
-// @description    Open the full size image when you click the link of that image's page in tab like view. Click toggles the view, right click closes it
-// @version        0.01
+// @description    Opens the full size image in tab like view. Offering three view states. Fitting, filling, original.
+// @version        0.07
 // @author         FlowrForWar
 // @include        *
 // @grant          GM_xmlhttpRequest
+// @grant          GM_setClipboard
 // @noframes
 // @license        MIT
 // ==/UserScript==
 
-const supported_websites = ['pixiv.net', 'flickr.com'];
-let parentDiv, div, image, state, scrollPosition, directLink, imageWidth, imageHeight;
-let ready = !1;
+const supported_websites = ['pixiv.net', 'flickr.com', 'imdb.com', 'reddit.com', 'redd.it', 'riotpixels.com'];
+let parentDiv, div, image, zoomDiv, span, scrollPosition, imageWidth, imageHeight, directLink, state, responseText, match, temp1, temp2, start, end, timeOut;
+
+async function get_image(target, closestAElement, href, childImgElement) {
+	switch (!0) {
+		// -----------------------------------------------------------------------
+		// pixiv.net
+		case /https:\/\/www\.pixiv\.net\/(en\/)?artworks\/\d+?$/.test(href):
+			toggle_tab(!0);
+			responseText = (await async_request(href)).responseText;
+			match = responseText.match(/meta name="preload-data" id="meta-preload-data" content='([^']+)'/);
+			if (match) {
+				temp1 = JSON.parse(match[1]).illust;
+				temp2 = temp1[Object.keys(temp1)[0]];
+				directLink = temp2.urls.original;
+			}
+			break;
+		// -----------------------------------------------------------------------
+		// flickr.com
+		case /https:\/\/www\.flickr\.com\/photos\/[^/]+\/\d/.test(href):
+			toggle_tab(!0);
+			responseText = (await async_request(href)).responseText;
+			match = responseText.match(/sizesForAlbum":(\{.+\}),"visibilitySource/);
+			if (match) {
+				temp1 = JSON.parse(match[1]);
+				temp2 = temp1.o || temp1.k || temp1.h || temp1.l || temp1.c || temp1.z || temp1.m || temp1.w || temp1.n || temp1.s;
+				directLink = temp2.url;
+			}
+			break;
+		// -----------------------------------------------------------------------
+		// imdb.com
+		case /https:\/\/www\.imdb\.com\/(name|title)\/[^/]+\/mediaviewer\//.test(href):
+			toggle_tab(!0);
+			temp1 = (childImgElement || target.closest('.ipc-photo').querySelector('img')).src;
+			directLink = temp1.replace(/\._V1.+/, '');
+			break;
+		// -----------------------------------------------------------------------
+		// reddit.com
+		case /https:\/\/www\.reddit\.com\/r\/[^/]+\/comments\//.test(href):
+			if (childImgElement && /https:\/\/(preview|i)\.redd\.it\//.test(childImgElement.src)) {
+				toggle_tab(!0);
+				directLink = childImgElement.src.replace(/https:\/\/preview\.redd\.it\/([^?]+)\?.+/, 'https://i.redd.it/$1');
+			}
+			break;
+		// -----------------------------------------------------------------------
+		// redd.it
+		case /https:\/\/(preview|i)\.redd\.it\//.test(href):
+			toggle_tab(!0);
+			directLink = href.replace(/https:\/\/preview\.redd\.it\/([^?]+)\?.+/, 'https://i.redd.it/$1');
+			break;
+		// -----------------------------------------------------------------------
+		// riotpixels.com
+		case /riotpixels\.com\/games\//.test(href):
+			if (childImgElement) {
+				toggle_tab(!0);
+				directLink = childImgElement.src.replace('jpg.240p.', '');
+			}
+			break;
+	}
+
+	if (!directLink) {
+		if (!closestAElement.dataset.fake && !(end - start > 500)) location.href = href;
+		return;
+	}
+	image.setAttribute('src', directLink);
+	image.style.setProperty('display', 'none');
+
+	// Seems to be the fastest way to get image.naturalWidth and image.naturalHeight
+	const hack_1 = setInterval(() => {
+		if (image.naturalWidth) {
+			imageWidth = Math.round(image.naturalWidth / window.devicePixelRatio);
+			imageHeight = Math.round(image.naturalHeight / window.devicePixelRatio);
+			clearInterval(hack_1);
+			image.style.removeProperty('display');
+			if ((imageWidth < innerWidth && imageHeight < innerHeight) || (imageWidth < innerWidth && imageHeight / imageWidth > 3.5)) original_view();
+			else fit_view();
+			tweaks();
+		}
+	}, 10);
+}
 
 function toggle_tab(show = !1) {
 	if (show) {
 		scrollPosition = window.scrollY;
 		document.body.style.setProperty('overflow', 'hidden', 'important');
 		parentDiv.style.removeProperty('display');
+		window.addEventListener('contextmenu', contextmenu);
 	} else {
 		parentDiv.style.setProperty('display', 'none');
+		zoomDiv.style.setProperty('display', 'none');
 		document.body.style.removeProperty('overflow');
 		window.scrollTo(0, scrollPosition);
 
 		image.removeAttribute('src');
 		image.removeAttribute('style');
-		directLink = null;
-		state = 'exit';
+		window.removeEventListener('contextmenu', contextmenu);
+		clearTimeout(timeOut);
+		span.innerHTML = '';
+		scrollPosition = imageWidth = imageHeight = directLink = state = responseText = match = temp1 = temp2 = start = end = timeOut = null;
 	}
 }
 
 function fill_view() {
-	console.log('Using fill view');
+	// console.log('Fill view');
 	image.setAttribute(
 		'style',
 		[
@@ -54,7 +136,7 @@ function fill_view() {
 }
 
 function original_view() {
-	console.log('Using origianl');
+	// console.log('Origianl View');
 	image.setAttribute(
 		'style',
 		[
@@ -76,7 +158,7 @@ function original_view() {
 }
 
 function fit_view() {
-	console.log('Using fit view');
+	// console.log('Fit view');
 	image.setAttribute(
 		'style',
 		[
@@ -96,115 +178,185 @@ function fit_view() {
 	state = 'fit';
 }
 
-function scale_image(event) {
-	if (state === 'fit') {
-		event.preventDefault();
-		fill_view();
-	} else if (state === 'fill') {
-		if (imageWidth <= innerWidth) event.preventDefault();
-		original_view();
-	} else if (state === 'original') {
-		event.preventDefault();
-		fit_view();
-	}
+function tweaks() {
 	image.style.setProperty('margin', (image.height > window.innerHeight ? '0px ' : '') + 'auto');
+	span.innerHTML = Math.round((image.width / imageWidth) * 100) + '%';
+	zoomDiv.style.removeProperty('display');
+	timeOut = setTimeout(() => {
+		zoomDiv.style.setProperty('display', 'none');
+	}, 2500);
 }
 
-window.addEventListener('mousedown', event => {
-	if (!ready || event.button !== 1 || parentDiv.style.display === 'none') return;
-	scale_image(event);
-});
+function scale_image(event) {
+	clearTimeout(timeOut);
+	switch (state) {
+		case 'fit':
+			event.preventDefault();
+			fill_view();
+			break;
+		case 'fill':
+			if (imageWidth <= innerWidth) event.preventDefault();
+			original_view();
+			break;
+		case 'original':
+			event.preventDefault();
+			fit_view();
+			break;
+	}
+	tweaks();
+}
 
-window.addEventListener(
-	'click',
-	async event => {
-		if (parentDiv.style.display !== 'none') {
-			scale_image(event);
-			return;
-		}
-
-		const closestAElement = event.target.closest('a');
-
-		if (!ready || !closestAElement || !closestAElement.href || closestAElement.getAttribute('href') === '#' || closestAElement.getAttribute('href').startsWith('javascript')) return;
-
-		event.preventDefault();
-		event.stopPropagation();
-
-		const href = closestAElement.href;
-		const website = get_website(href);
-		// const childImgElement = closestAElement.querySelector('img');
-
-		if (!supported_websites.includes(website)) {
-			location.href = href;
-			return;
-		}
-
-		if (/https:\/\/www\.pixiv\.net\/(en\/)?artworks\/\d+?$/.test(href)) {
-			toggle_tab(!0);
-			const responseText = (await request(href)).responseText;
-			const match = responseText.match(/meta name="preload-data" id="meta-preload-data" content='([^']+)'/);
-			if (match) {
-				const temp = JSON.parse(match[1]).illust;
-				const illust = temp[Object.keys(temp)[0]];
-				if (illust.pageCount !== 1 && confirm('There are more images on this link, Open the page as well?')) window.open(href);
-				directLink = illust.urls.original;
-			}
-		} else if (/https:\/\/www\.flickr\.com\/photos\/[^/]+\/\d/.test(href)) {
-			toggle_tab(!0);
-			const responseText = (await request(href)).responseText;
-			const match = responseText.match(/sizesForAlbum":(\{.+\}),"visibilitySource/);
-			if (match) {
-				const photos = JSON.parse(match[1]);
-				const best_photo = photos.o || photos.k || photos.h || photos.l || photos.c || photos.z || photos.m || photos.w || photos.n || photos.s;
-				directLink = best_photo.url;
-			}
-		}
-
-		if (!directLink) {
-			location.href = href;
-			return;
-		}
-		image.setAttribute('src', directLink);
-		image.style.setProperty('display', 'none');
-
-		// Seems to be the fastest way to get image.naturalWidth and image.naturalHeight
-		const hack_1 = setInterval(() => {
-			if (image.naturalWidth) {
-				imageWidth = Math.round(image.naturalWidth / window.devicePixelRatio);
-				imageHeight = Math.round(image.naturalHeight / window.devicePixelRatio);
-				clearInterval(hack_1);
-				image.style.removeProperty('display');
-				if ((imageWidth < innerWidth && imageHeight < innerHeight) || (imageWidth < innerWidth && imageHeight / imageWidth > 3.5)) original_view();
-				else fit_view();
-				image.style.setProperty('margin', (image.height > window.innerHeight ? '0px ' : '') + 'auto');
-			}
-		}, 10);
-	},
-	!0
-);
-
-window.addEventListener('contextmenu', event => {
-	if (!ready || event.shiftKey || parentDiv.style.display === 'none') return;
-	event.preventDefault();
-	toggle_tab();
-});
-
-function start() {
+function create_viewer() {
 	parentDiv = document.createElement('div');
-	parentDiv.setAttribute('style', 'position:fixed;background-color: #1f1f1f;z-index:3000;top:0;left:0;bottom:0;right:0;overflow: auto;display:none');
+	parentDiv.setAttribute('style', 'position:fixed; background-color:#1f1f1f; z-index:30000; inset:0; overflow:auto; display:none');
 	parentDiv.setAttribute('id', 'pretending-tab');
-
 	div = document.createElement('div');
-	div.setAttribute('style', 'height: auto;width: max-content;background-color: #1f1f1f;position: absolute;top: 0;left: 0;bottom: 0;right: 0;');
+	div.setAttribute('style', 'height:auto; width:max-content; background-color:#1f1f1f; position:absolute; inset:0');
 	parentDiv.appendChild(div);
 	image = document.createElement('img');
 	div.appendChild(image);
 	document.documentElement.appendChild(parentDiv);
-	ready = !0;
+
+	zoomDiv = document.createElement('div');
+	zoomDiv.setAttribute('style', 'position:fixed; background-color:#000; z-index:30001; bottom:10px; right:10px; padding:10px; border-radius:5px; display:none');
+	zoomDiv.setAttribute('id', 'pretending-tab-zoom-level');
+	span = document.createElement('span');
+	span.setAttribute('style', 'font-size:large; font-family:sans-serif; color:#fff');
+	zoomDiv.appendChild(span);
+	document.documentElement.appendChild(zoomDiv);
+
+	window.addEventListener('mousedown', mousedown);
+	window.addEventListener('click', click, !0);
 }
 
-if (document.readyState !== 'loading') start();
-else document.addEventListener('DOMContentLoaded', start);
+if (document.readyState !== 'loading') create_viewer();
+else document.addEventListener('DOMContentLoaded', create_viewer);
+
+// -----------------------------------------------------------------------
+// Gestures part of the code is by Peer Zeng
+// https://greasyfork.org/en/scripts/4776-my-mouse-gestures
+
+const gestures = {
+	U: function() {
+		parentDiv.scrollTo(0, 0);
+	},
+	D: function() {
+		parentDiv.scrollTo(0, 1073741824);
+	},
+	LR: function() {
+		window.open(image.src);
+	},
+	RL: function() {
+		GM_setClipboard(image.src);
+	},
+	// RD: function() {},
+};
+
+const SENSITIVITY = 3; // 1 ~ 5
+const TOLERANCE = 3; // 1 ~ 5
+
+const s = 1 << ((7 - SENSITIVITY) << 1);
+const t1 = Math.tan(0.15708 * TOLERANCE),
+	t2 = 1 / t1;
+
+let x, y, path;
+
+const tracer = function(e) {
+	let cx = e.clientX,
+		cy = e.clientY,
+		deltaX = cx - x,
+		deltaY = cy - y,
+		distance = deltaX * deltaX + deltaY * deltaY;
+	if (distance > s) {
+		let slope = Math.abs(deltaY / deltaX),
+			direction = '';
+		if (slope > t1) {
+			direction = deltaY > 0 ? 'D' : 'U';
+		} else if (slope <= t2) {
+			direction = deltaX > 0 ? 'R' : 'L';
+		}
+		if (path.charAt(path.length - 1) !== direction) {
+			path += direction;
+		}
+		x = cx;
+		y = cy;
+	}
+};
+
+// -----------------------------------------------------------------------
+
+function click(event) {
+	if (parentDiv.style.display !== 'none') {
+		scale_image(event);
+		return;
+	}
+
+	end = performance.now();
+	const { target } = event;
+
+	let closestAElement;
+	if (target.closest('a')) closestAElement = target.closest('a');
+	else if (target.tagName === 'IMG' && target.src && target.src.startsWith('http')) {
+		closestAElement = document.createElement('a');
+		closestAElement.setAttribute('href', target.src);
+		closestAElement.dataset.fake = !0;
+	}
+
+	if (!closestAElement || !closestAElement.href || closestAElement.getAttribute('href') === '#' || closestAElement.getAttribute('href').startsWith('javascript')) {
+		return;
+	}
+
+	const href = closestAElement.href;
+	const website = get_website(href);
+
+	if (!supported_websites.includes(website)) {
+		if (end - start > 500) {
+			event.preventDefault();
+			event.stopPropagation();
+			event.stopImmediatePropagation();
+		} else if (!closestAElement.dataset.fake) {
+			location.href = href;
+		}
+		return;
+	}
+
+	event.preventDefault();
+	event.stopPropagation();
+	event.stopImmediatePropagation();
+
+	const childImgElement = closestAElement.querySelector('img');
+	get_image(target, closestAElement, href, childImgElement);
+}
+
+function contextmenu(event) {
+	end = performance.now();
+	window.removeEventListener('mousemove', tracer, false);
+	if (event.shiftKey || end - start > 500) return;
+	event.preventDefault();
+	if (path !== '' && gestures.hasOwnProperty(path)) gestures[path]();
+	else toggle_tab();
+}
+
+function mousedown(event) {
+	switch (event.button) {
+		// Left Click
+		case 0:
+			start = performance.now();
+			break;
+		// Middle Click
+		case 1:
+			if (parentDiv.style.display !== 'none') scale_image(event);
+			break;
+		// Right Click
+		case 2:
+			start = performance.now();
+			x = event.clientX;
+			y = event.clientY;
+			path = '';
+			window.addEventListener('mousemove', tracer, false);
+			break;
+	}
+}
 
 function get_website(href) {
 	try {
@@ -218,7 +370,7 @@ function get_website(href) {
 	}
 }
 
-function request(url) {
+function async_request(url) {
 	return new Promise(resolve => {
 		GM_xmlhttpRequest({ method: 'GET', url, onload: response => resolve(response) });
 	});
